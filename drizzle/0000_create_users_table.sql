283ab23b344b127206b2161cd13238bc6e286b48
Amine Takha <48015352+aminetakha@users.noreply.github.com>
2025-05-11 18:09:10 +0100

    Added user table

---
+++ drizzle/0000_create_users_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"first_name" varchar(100) NOT NULL,
+	"last_name" varchar(100) NOT NULL,
+	"email" varchar NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"salt" varchar(255) NOT NULL,
+	"phone" varchar(20) NOT NULL,
+	"role" varchar(10) NOT NULL,
+	"city" varchar(100) NOT NULL,
+	"state" varchar(100) NOT NULL,
+	"zip_code" integer NOT NULL,
+	"image_url" varchar,
+	"created_at" date DEFAULT now() NOT NULL,
+	"updated_at" date DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import { date, integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  firstName: varchar("first_name", { length: 100 }).notNull(),
+  lastName: varchar("last_name", { length: 100 }).notNull(),
+  email: varchar().notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  salt: varchar({ length: 255 }).notNull(),
+  phone: varchar({ length: 20 }).notNull(),
+  role: varchar({
+    enum: ["buyer", "seller", "mechanic", "admin"],
+    length: 10,
+  }).notNull(),
+  city: varchar({ length: 100 }).notNull(),
+  state: varchar({ length: 100 }).notNull(),
+  zipCode: integer("zip_code").notNull(),
+  imageUrl: varchar("image_url"),
+  createdAt: date("created_at").defaultNow().notNull(),
+  updatedAt: date("updated_at").defaultNow().notNull(),
+});
